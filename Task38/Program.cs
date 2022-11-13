c29c5abad70d0e6d333c6e611ba5823832d08512
Michkasov Roman <mihjob@list.ru>
2022-11-13 19:00:31 +0300

    Task38

---
+++ Task38/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+//Найдите разницу между максимальным и минимальным элементов массива.
+
+double[] Returns_Array(int size, int LeftValue, int RightValue)
+{
+    double[] vector = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        vector[i] = new Random().Next(LeftValue, RightValue + 1);
+    }
+    return vector;
+}
+
+double MaxElement (double[] array)
+{
+    double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
+
+double MinElement (double[] array)
+{
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    return min;
+}
+
+Console.Clear();
+
+Console.Write("Введите размер массива: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение левого конца диапазона: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение правого конца диапазона: ");
+int b = int.Parse(Console.ReadLine()!);
+double[] WorkingArray = Returns_Array(number, a, b);
+Console.WriteLine($"Полученный массив: [{String.Join(", ", WorkingArray)}]");
+
+double sum = MaxElement(WorkingArray) + MinElement(WorkingArray);
+Console.WriteLine($"Сумма наименьшего и наибольшего элементов массива равна {sum}");
